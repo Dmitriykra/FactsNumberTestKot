4fa231d37f9d04120a47a57414d4b45a44d7d975
Dmytro Kravtsov <kravtsovdimitri@gmail.com>
2022-12-23 12:58:01 +0200

    v1

---
+++ app/src/main/java/dmytro/app/factsnumbertestkot/MainActivity.kt
@@ -0,0 +1,12 @@
+package dmytro.app.factsnumbertestkot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
